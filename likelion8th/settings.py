8aee54bb0f4efe1e527768fe916330f3a72145f1
dain Hong <ribbon0522@naver.com>
2020-05-25 14:26:21 +0900

    (4주차)blog세션

--- likelion8th/settings.py
+++ likelion8th/settings.py
@@ -119,4 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' #static 파일을 불러올때 url
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog', 'static')] #static 파일 있는 경로를 작성
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') # static 파일을 한곳에 모을때, 모아줄 위치
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') #media 파일이 저장되는 위치
+MEDIA_URL = '/media/' #미디어파일 요청 받을 url주소

1df82e3232d2e1300c81f6e0e6251d1a8e452790
dain Hong <ribbon0522@naver.com>
2020-07-07 17:11:06 +0900

    (7주차)정규세션

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render,get_object_or_404, redirect
-from .models import Blog
+from django.shortcuts import render,get_object_or_404,redirect
+from .models import *
 from django.utils import timezone
 from .form import BlogForm
 # Create your views here.
@@ -10,8 +10,39 @@ def list(request):
 
 def detail(request,blog_id):
     blog = get_object_or_404(Blog, pk = blog_id)
-    return render(request, 'detail.html',{'blog':blog})
+    like_num = len(blog.like.all())
+    comments =Comment.objects.filter(blog=blog)
+    return render(request, 'detail.html',{'blog':blog, 'comments': comments, 'likes':like_num})
 
+def commenting(request, blog_id):
+    new_comment = Comment()
+    new_comment.blog = get_object_or_404(Blog, pk = blog_id)
+    new_comment.author = request.user
+    new_comment.body = request.POST.get('body')
+    new_comment.blog_id = blog_id
+    new_comment.save()
+
+    print('create test')
+    return redirect('/blog/'+ str(blog_id))
+
+# def edit_commenting(request,comment_id):
+#     commenting = get_object_or_404(Comment, pk = comment_id)
+#     return render(request, '/blog/update_commenting.html')
+
+# def update_commenting(request,comment_id):
+#     edit_commenting = get_object_or_404(Comment, pk = comment_id)
+#     edit_commenting.author = request.user
+#     edit_commenting.body = request.POST.get('body')
+#     edit_commenting.save()
+#     return redirect('blog:detail',blog_id)
+
+
+def like(request, blog_id):
+    blog = get_object_or_404(Blog, pk = blog_id)
+    blog.like.add(request.user)
+    blog.save()
+
+    return redirect('/blog/' + str(blog_id))
 def new(request):
 #1. 데이터가 입력된 후 제출 버튼을 누르고 데이터 저장 = post
 #2. 정보가 입력되지 않은 빈칸으로 되어있는 페이지 보여주기 = get
